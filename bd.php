6471fc4630ea04c4b2aaf9eb4546e1c56eb7c702
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2018-03-20 10:29:38 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,11 +2,25 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:caramba8787.database.windows.net,1433; Database = rewq", "Caramba8787", "535412danNN");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE  `users` (
+ `id` INT NOT NULL AUTO_INCREMENT ,
+ `login` VARCHAR( 20 ) NOT NULL ,
+ `password` VARCHAR( 35 ) NOT NULL ,
+ `email` VARCHAR( 50 ) NOT NULL ,
+ `reg_date` VARCHAR( 32 ) NOT NULL ,
+ `name_user` VARCHAR( 32 ) NOT NULL ,
+ `lastname` VARCHAR( 32 ) NOT NULL ,
+PRIMARY KEY (  `id` )
+)";
+
+$conn->quary($sql);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
+
 
 
 ?>
