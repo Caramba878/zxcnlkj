05be3942a81feaf98d2e3c494130006195bd5de2
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2018-03-20 10:04:08 +0300

    Create registration1.php

---
+++ registration1.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Регистрация проверки</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="http://vladmaxi.net/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+
+<!-- vladmaxi top bar -->
+    
+<!--/ vladmaxi top bar -->
+
+<form id="login">
+    <h1>Регистрация</h1>
+    <fieldset id="inputs">
+        <input id="username" type="text" placeholder="Логин" autofocus required>
+        <input id="usernamename" type="text" placeholder="Имя" autofocus required>
+        <input id="usernamefamily" type="text" placeholder="Фамилия" autofocus required>
+        <input id="password" type="password" placeholder="Пароль" required>
+        <input id="password2" type="password" placeholder="Повторите пароль" required>
+    </fieldset>
+    <fieldset id="actions">
+        <input type="submit" id="submit" value="Зарегестрироваться">
+        
+    </fieldset>
+</form>
+</body>
+</html>
