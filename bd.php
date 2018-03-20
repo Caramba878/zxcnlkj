c0b85cc24760b62b68ca92cdbf110ca5878e6159
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2018-03-20 10:24:34 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,11 +1,12 @@
 <?php
-session_start();
+try {
+    $conn = new PDO("sqlsrv:server = tcp:caramba8787.database.windows.net,1433; Database = rewq", "Caramba8787", "535412danNN");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-mysql_connect ("localhost","jexa","12345");
-mysql_select_db ("reg");
-mysql_query("SET NAMES utf8");
 
-$login = $_SESSION['login'];
-$password = $_SESSION['password'];
-$id_user = $_SESSION['id'];
 ?>
