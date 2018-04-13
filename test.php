f88de07c69e810337af4da8631af23d5b0b604c4
Caramba878 <32097727+Caramba878@users.noreply.github.com>
2018-04-13 20:26:39 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<div class="text">
+        <form action="test.php" method="post">
+                            <span style="font-weight: 900;">1.Какой Ваш возраст?:</span>
+                            <select name = "cards">
+                            <option value = '1'></option>
+                                 <option value = '3'></option>
+                                      <option value = '2'></option>
+                            </select>
+               
+                        <div style="text-align: center">
+            <input class="btn btn-success btn-lg" type="submit" value="Получить результат">
+            </div>
